e5db40569ae4c8a8ac485bfeb254bc1f50bd5776
George Ho <georgeho1618@gmail.com>
2018-04-16 21:14:01 -0400

    BLD add create_w2v

--- create_word2vec.py
+++ create_word2vec.py
@@ -3,9 +3,16 @@ from gensim.models.word2vec import PathLineSentences, Word2Vec
 
 
 if __name__ == '__main__':
-    sentences = PathLineSentences(os.getcwd() + '/data/corpus1/train')
-    w2v = Word2Vec(sentences,
-                   size=100,
-                   window=5,
-                   seed=1618)
-    w2v.save('w2v.100d.txt')
+    DATA_DIRS = ['/data/corpus1/total',
+                 '/data/corpus2/train',
+                 '/data/corpus3/train']
+
+    for idx, data_dir in enumerate(DATA_DIRS):
+        sentences = PathLineSentences(os.getcwd() + data_dir)
+        w2v = Word2Vec(sentences,
+                       size=100,
+                       min_count=1,
+                       window=5,
+                       seed=1618,
+                       iter=100)
+        w2v.save('w2v.corpus{}.100d'.format(idx))
